ba0dc4df866a9cbf33a49ac88fde1197f6147bc8
Andrew <andrew.panix@gmail.com>
2019-04-07 10:13:03 +0300

    init

--- views/admin/settings/index.php
+++ views/admin/settings/index.php
@@ -1,37 +1,33 @@
 <?php
 
 use panix\engine\Html;
-use yii\widgets\ActiveForm;
+use panix\engine\bootstrap\ActiveForm;
 use panix\mod\shop\models\ProductType;
 use yii\helpers\ArrayHelper;
+
 ?>
-<?php
-$form = ActiveForm::begin([
+
+<div class="card bg-light">
+    <div class="card-header">
+        <h5><?= $this->context->pageName ?></h5>
+    </div>
+    <div class="card-body">
+        <?php
+        $form = ActiveForm::begin([
             //  'id' => 'form',
-            'options' => ['class' => 'form-horizontal'],
-            'fieldConfig' => [
-                'template' => "{label}\n<div class=\"col-sm-7\">{input}</div>\n<div class=\"col-sm-7 col-sm-offset-5\">{error}</div>",
-                'labelOptions' => ['class' => 'col-sm-5 control-label'],
-            ],
+          //  'options' => ['class' => 'form-horizontal'],
         ]);
-?>
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h3 class="panel-title"><?= $this->context->pageName ?></h3>
-    </div>
-    <div class="panel-body">
+        ?>
         <?= $form->field($model, 'pagenum') ?>
+        <?= $form->field($model, 'use_type')->dropDownList(ArrayHelper::map(ProductType::find()->all(), 'id', 'name'), [
+            'prompt' => 'Укажите тип товара'
+        ])->hint('Если не выбрать тип, то параметр "<b>type</b>" станет обязательный для csv файла.'); ?>
 
-
-<?= $form->field($model, 'use_type')->dropDownList(ArrayHelper::map(ProductType::find()->all(), 'id', 'name'), [
-    'prompt' => 'Укажите производителя'
-]); ?>
-Если не выбрать тип, то параметр "<b>type</b>" станет обязательный для csv файла.
+        <?php ActiveForm::end(); ?>
     </div>
-    <div class="panel-footer text-center">
+    <div class="card-footer text-center">
 
         <?= Html::submitButton(Html::icon('check') . ' ' . Yii::t('app', 'SAVE'), ['class' => 'btn btn-success']) ?>
 
     </div>
 </div>
-<?php ActiveForm::end(); ?>

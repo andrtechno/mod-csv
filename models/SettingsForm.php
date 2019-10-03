05df0666bec26b350af0d477d354db6504c84cc1
Andrew <andrew.panix@gmail.com>
2019-10-03 03:40:21 +0300

    init

--- models/SettingsForm.php
+++ models/SettingsForm.php
@@ -1,17 +1,31 @@
 <?php
+
 namespace panix\mod\csv\models;
-class SettingsForm extends \panix\engine\SettingsModel {
+
+use panix\engine\SettingsModel;
+
+class SettingsForm extends SettingsModel
+{
 
     protected $module = 'csv';
+    public static $category = 'csv';
 
     public $use_type;
     public $pagenum;
 
-    public function rules() {
+    public function rules()
+    {
         return [
             ['use_type', 'string'],
             ['pagenum', 'required'],
         ];
     }
 
+    public static function defaultSettings()
+    {
+        return [
+            'pagenum' => 10,
+            'use_type' => ''
+        ];
+    }
 }

426ae42006f584a78ae70fc0e7d1b8d8b6659143
Andrew <andrew.panix@gmail.com>
2020-01-30 01:38:09 +0200

    init

--- controllers/admin/SettingsController.php
+++ controllers/admin/SettingsController.php
@@ -24,7 +24,9 @@ class SettingsController extends AdminController
         if ($model->load(Yii::$app->request->post())) {
             if($model->validate()){
                 $model->save();
+                Yii::$app->session->setFlash("success", Yii::t('app/default', 'SUCCESS_UPDATE'));
             }
+            return $this->refresh();
         }
 
         return $this->render('index', ['model' => $model]);
